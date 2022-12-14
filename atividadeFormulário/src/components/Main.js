e10f229eb6d600cd81559d225b8dcfab51f36181
Jose Marcus <josemarcus@academico.ifpb.edu.br>
2022-12-14 12:31:38 -0300

    atividadeFormulário

---
+++ atividadeFormulário/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Clientes from './Clientes'
+
+const Main = () => {
+  return (
+    <>
+    <Clientes></Clientes>
+
+    </>
+    
+  );
+};
+
+export default Main;
